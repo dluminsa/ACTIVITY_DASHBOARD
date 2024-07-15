6bdccc8c02e44b4d6db8777de07cdd60a1b6360e
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-07-15 16:00:13 +0300

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -110,7 +110,7 @@ else:
 ar = filtered_dfb['AREA']. unique()
 if len(ar) == 0:
     cold.write(f'**No data for the thematic area(s) chosen**')
-elif len(ar)>1
+elif len(ar)>1:
      pass
 elif len(ar)==1:
      cold.write(f'**The data set is filtered by: {ar} thematic area(s)**')
