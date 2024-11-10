6cb0deebe1b24846c2946937812d77d8c01823e8
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-10 20:18:13 +0300

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -212,10 +212,10 @@ for ary in areas:
                plan = allyp['PLANNED'].sum()
                conducted = ally['DONE'].sum()
                notdone = plan - conducted
-               cola.write(f'**{district}**)
-               colb.write(f'**{plan}**)
-               colc.write(f'**{conducted}**)
-               cold.write(f'**{notdone}**)
+               cola.write(f'**{district}**')
+               colb.write(f'**{plan}**')
+               colc.write(f'**{conducted}**')
+               cold.write(f'**{notdone}**')
      elif chec==1:
           cola, colb, colc = st.columns([2,1,1,1])
           cola.write('**FACILITY**')
@@ -223,8 +223,8 @@ for ary in areas:
           for fact in facys:
                ally = allya[allya['DISTRICT']==fact]
                conducted = ally['DONE'].sum()
-               cola.write(f'**{district}**)
-               colc.write(f'**{conducted}**)
+               cola.write(f'**{district}**')
+               colc.write(f'**{conducted}**')
      else:
          pass                 
                           
