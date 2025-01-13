a0bf02d47fda9ebb00e30325b7c5a4c34f0a0868
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-14 00:25:15 +0300

    Update STATUS.py

--- pages/STATUS.py
+++ pages/STATUS.py
@@ -82,7 +82,7 @@ if Intention == 'MARK REVIEWED PAPER WORK':
     else:
           st.session_state.sear = True
 
-if st.session_state.sear:
+    if st.session_state.sear:
         try:
              conn = st.connection('gsheets', type=GSheetsConnection)
              exist1 = conn.read(worksheet= 'DONE', usecols=list(range(12)),ttl=5)
@@ -93,111 +93,108 @@ if st.session_state.sear:
              st.write("POOR INTERNET, COULDN'T CONNECT TO THE GOOGLE SHEETS")
              st.write('Get better internet and try again')
              st.stop()
-if st.session_state.sear:
-    review = existing1[existing1['CLUSTER'] == cluster].copy()
-    review['ID'] = pd.to_numeric(review['ID'], errors='coerce')
-    @st.cache_data
-    def finder ():
-         idx = []
-         idx = [int(i) for i in ids]
-         dfsee = review[review['ID'].isin(idx)].copy()
-         return dfsee
-    dfa = finder() 
-    if dfa.shape[0] == 0:
-         st.warning("**ID(s) NOT FOUND**")
-         st.stop()
-    else:
-         pass
-    st.write('**FIRST CHECK THEM BEFORE SUBMISSION**')
-    dfa = dfa[['DISTRICT', 'FACILITY', 'ACTIVITY', 'ID','AMOUNT']].copy()
-    dfu = dfa.copy()
-    dfa.index = pd.Index(range(1, len(dfa) + 1))
-    st.write(dfa)
-    a = dfa.shape[0]
-    b = len(ids)
-    if a == b:
-         pass
-    elif b>a:
-         ad = dfa['ID'].tolist()
-         abc = ids
-         ab = set(abc) - set(ad)
-         leg = len(list(ab))
-         abs = [str(i) for i in ab]
-         ab = ','.join(list(abs))
-         if leg ==1:
-              st.warning(f'**UNIQUE ID {ab} WAS NOT FOUND**')   
-         elif leg > 1:
-              st.warning(f'**THESE UNIQUE ID WERE NOT FOUND: {ab}**')
-              proc = st.radio('**DO YOU WANT TO PROCEED TO SUBMIT WITHOUT THEM**', options= ['YES', 'NO'], horizontal=True, index=None)
-              if not proc:
-                   st.session_state.sear = True
+    if st.session_state.sear:
+              review = existing1[existing1['CLUSTER'] == cluster].copy()
+              review['ID'] = pd.to_numeric(review['ID'], errors='coerce')
+              @st.cache_data
+              def finder ():
+                   idx = []
+                   idx = [int(i) for i in ids]
+                   dfsee = review[review['ID'].isin(idx)].copy()
+                   return dfsee
+              dfa = finder() 
+              if dfa.shape[0] == 0:
+                   st.warning("**ID(s) NOT FOUND**")
                    st.stop()
-              elif proc == 'NO':
-                   st.write('**REFRESH TO SEARCH AGAIN OR REMOVE IT FROM THE LIST ABOVE**')
-                   st.session_state.sear = False
-                   st.stop
               else:
-                   st.session_state.sear = True
-         else:
-             st.session_state.sear = True
-    submit = st.button('**SUBMIT**')  
-    if not submit:
-         st.stop()
-    else:
-         pass
-    if submit:
-            secrets = st.secrets["connections"]["gsheets"]
-            credentials_info = {
-                       "type": secrets["type"],
-                       "project_id": secrets["project_id"],
-                       "private_key_id": secrets["private_key_id"],
-                       "private_key": secrets["private_key"],
-                       "client_email": secrets["client_email"],
-                       "client_id": secrets["client_id"],
-                       "auth_uri": secrets["auth_uri"],
-                       "token_uri": secrets["token_uri"],
-                       "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
-                       "client_x509_cert_url": secrets["client_x509_cert_url"]
-                   }
-                       
-            try:
-                   # Define the scopes needed for your application
-                   scopes = ["https://www.googleapis.com/auth/spreadsheets",
-                           "https://www.googleapis.com/auth/drive"]
-                   
-                    
-                   credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
-                       
-                       # Authorize and access Google Sheets
-                   client = gspread.authorize(credentials)
-                       
-                       # Open the Google Sheet by URL
-                   spreadsheetu = " https://docs.google.com/spreadsheets/d/1IgIltX9_2yvppb4YYoebRyyYwCqYZng62h0cRYPmAdE"     
-                   spreadsheet = client.open_by_url(spreadsheetu)
-            except Exception as e:   
-                   st.write(f"CHECK: {e}")
-                   st.write(traceback.format_exc())
-                   st.write("COULDN'T CONNECT TO GOOGLE SHEET, TRY AGAIN")
+                   pass
+              st.write('**FIRST CHECK THEM BEFORE SUBMISSION**')
+              dfa = dfa[['DISTRICT', 'FACILITY', 'ACTIVITY', 'ID','AMOUNT']].copy()
+              dfu = dfa.copy()
+              dfa.index = pd.Index(range(1, len(dfa) + 1))
+              st.write(dfa)
+              a = dfa.shape[0]
+              b = len(ids)
+              if a == b:
+                   pass
+              elif b>a:
+                   ad = dfa['ID'].tolist()
+                   abc = ids
+                   ab = set(abc) - set(ad)
+                   leg = len(list(ab))
+                   abs = [str(i) for i in ab]
+                   ab = ','.join(list(abs))
+                   if leg ==1:
+                        st.warning(f'**UNIQUE ID {ab} WAS NOT FOUND**')   
+                   elif leg > 1:
+                        st.warning(f'**THESE UNIQUE ID WERE NOT FOUND: {ab}**')
+                        proc = st.radio('**DO YOU WANT TO PROCEED TO SUBMIT WITHOUT THEM**', options= ['YES', 'NO'], horizontal=True, index=None)
+                        if not proc:
+                             st.session_state.sear = True
+                             st.stop()
+                        elif proc == 'NO':
+                             st.write('**REFRESH TO SEARCH AGAIN OR REMOVE IT FROM THE LIST ABOVE**')
+                             st.session_state.sear = False
+                             st.stop
+                        else:
+                             st.session_state.sear = True
+                   else:
+                       st.session_state.sear = True
+              submit = st.button('**SUBMIT**')  
+              if not submit:
                    st.stop()
-    if submit: 
-            try:
-               st. write('SUBMITING')
-               sheet1 = spreadsheet.worksheet("PAID")
-               rows_to_append = dfu.values.tolist()
-               sheet1.append_rows(rows_to_append, value_input_option='RAW')
-               st.success('Your data above has been submitted')
-               st.write('RELOADING PAGE')
-               time.sleep(3)
-               st.markdown("""
-               <meta http-equiv="refresh" content="0">
-                    """, unsafe_allow_html=True)
-
-            except:
-               st.write("Couldn't submit, poor network") 
+              else:
+                   pass
+              if submit:
+                      secrets = st.secrets["connections"]["gsheets"]
+                      credentials_info = {
+                                 "type": secrets["type"],
+                                 "project_id": secrets["project_id"],
+                                 "private_key_id": secrets["private_key_id"],
+                                 "private_key": secrets["private_key"],
+                                 "client_email": secrets["client_email"],
+                                 "client_id": secrets["client_id"],
+                                 "auth_uri": secrets["auth_uri"],
+                                 "token_uri": secrets["token_uri"],
+                                 "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+                                 "client_x509_cert_url": secrets["client_x509_cert_url"]
+                             }
+                                 
+                      try:
+                             # Define the scopes needed for your application
+                             scopes = ["https://www.googleapis.com/auth/spreadsheets",
+                                     "https://www.googleapis.com/auth/drive"]
+                             
+                              
+                             credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
+                                 
+                                 # Authorize and access Google Sheets
+                             client = gspread.authorize(credentials)
+                                 
+                                 # Open the Google Sheet by URL
+                             spreadsheetu = " https://docs.google.com/spreadsheets/d/1IgIltX9_2yvppb4YYoebRyyYwCqYZng62h0cRYPmAdE"     
+                             spreadsheet = client.open_by_url(spreadsheetu)
+                      except Exception as e:   
+                             st.write(f"CHECK: {e}")
+                             st.write(traceback.format_exc())
+                             st.write("COULDN'T CONNECT TO GOOGLE SHEET, TRY AGAIN")
+                             st.stop()
+                      try:
+                         st. write('SUBMITING')
+                         sheet1 = spreadsheet.worksheet("PAID")
+                         rows_to_append = dfu.values.tolist()
+                         sheet1.append_rows(rows_to_append, value_input_option='RAW')
+                         st.success('Your data above has been submitted')
+                         st.write('RELOADING PAGE')
+                         time.sleep(3)
+                         st.markdown("""
+                         <meta http-equiv="refresh" content="0">
+                              """, unsafe_allow_html=True)
+          
+                      except:
+                         st.write("Couldn't submit, poor network") 
                  
-
 else:
-
     clusters  = df['CLUSTER'].unique()
     cluster = st.radio("**Choose a cluster:**", clusters,horizontal=True, index=None)
     if cluster:
