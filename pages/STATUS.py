cdfd7b0247959b90429b8d9c181c4f5b7fe6e683
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-13 23:23:24 +0300

    Update STATUS.py

--- pages/STATUS.py
+++ pages/STATUS.py
@@ -111,7 +111,8 @@ if Intention == 'MARK REVIEWED PAPER WORK':
          pass
     elif b>a:
          ad = dfa['ID'].tolist()
-         abc = idx
+         st.write(ids)
+         abc = ids
          ab = set(abc) - set(ad)
          st.warning(f'**THESE UNIQUE ID WERE NOT FOUND: {ab}**')
          proc = st.radio('**DO YOU WANT TO PROCEED TO SUBMIT WITHOUT THEM**', options= ['YES', 'NO'], horizontal=True, index=None)
