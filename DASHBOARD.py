81a97fbbf937b83e9745ac56aab6f6ab18398d68
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-11-17 20:58:11 +0300

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from streamlit_gsheets import GSheetsConnection
 import plotly.express as px
 import plotly.graph_objects as go
+st.stop()
 
 st.set_page_config(
      page_title= 'ACTIVITY DASHBOARD'
